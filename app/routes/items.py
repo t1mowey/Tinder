2e16120ca0e8244ce725446e9cae4c3c353e544a
Тимофей Прохоренко <timoxa-pro030303@yandex.ru>
2025-05-20 13:39:23 +0300

    Added random rout

--- app/routes/items.py
+++ app/routes/items.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from sqlalchemy import func
 from typing import List
 from ..database import get_db
 
@@ -8,12 +9,17 @@ from app import models, schema
 router = APIRouter(prefix="/items", tags=["items"])
 
 
-@router.get("/", response_model=List[schema.ProductRead])
-def read_items(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+@router.get("/all", response_model=List[schema.ProductRead])
+def read_all_items(limit: int = 100, db: Session = Depends(get_db)):
     items = db.query(models.Product).limit(limit).all()
     print("🔥 Деплой сработал!")
     return items
 
+
+@router.get('/random', response_model=List[schema.ProductRead])
+def read_random_items(limit: int = 100, db: Session = Depends(get_db)):
+    items = db.query(models.Product).order_by(func.random()).limit(limit).all()
+    return items
 
 
 
