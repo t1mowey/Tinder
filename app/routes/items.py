2a2018475d41b4ab9cbbbc73037cdb880ed35431
Тимофей Прохоренко <timoxa-pro030303@yandex.ru>
2025-05-20 12:59:25 +0300

    sql engine fixed

--- app/routes/items.py
+++ app/routes/items.py
@@ -9,7 +9,7 @@ router = APIRouter(prefix="/items", tags=["items"])
 
 
 @router.get("/", response_model=List[schema.ProductRead])
-def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+def read_items(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     items = db.query(models.Product).limit(limit).all()
     return items
 
