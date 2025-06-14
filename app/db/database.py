10e06073a47fbea277b8f742f747af3ee317b8c5
Тимофей Прохоренко <timoxa-pro030303@yandex.ru>
2025-06-14 17:56:45 +0300

    Notifications about Deploy

--- app/db/database.py
+++ app/db/database.py
@@ -5,7 +5,14 @@ import os
 
 load_dotenv()
 
-DATABASE_URL = os.environ.get('DATABASE_URL')
+# DATABASE_URL = os.environ.get('DATABASE_URL')
+
+DB_USER = os.environ.get('DB_USER')
+DB_PASSWORD = os.environ.get('DB_PASSWORD')
+DB_HOST = os.environ.get('DB_HOST')
+DB_PORT = os.environ.get('DB_PORT')
+DB_NAME = os.environ.get('DB_NAME')
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_async_engine(DATABASE_URL, echo=True)  # echo=True для логов SQL
 
@@ -17,7 +24,6 @@ Base = declarative_base()
 async def get_db():
     async with SessionLocal() as db:
         yield db
-
 
 
 
--- app/main.py
+++ app/main.py
@@ -9,3 +9,8 @@ app.include_router(authorization.auth)# подключаем роуты
 @app.get("/")
 async def root():
     return {"message": "Hello, check /docs to see all the routes."}
+
+
+@app.get("/deploy_ok")
+async def test_deploy():
+    return {'message': 'OK'}
